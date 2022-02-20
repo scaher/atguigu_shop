817646dd6036eb1abadea379e66b542383552209
scaher <1344384956@qq.com>
2022-02-20 11:07:20 +0800

    初步完成搜索页面动态显示

--- src/store/search/index.js
+++ src/store/search/index.js
@@ -12,13 +12,26 @@ const actions = {
     async getSearchInfo(context,params = {}){
         // params形参，当用户派发actions时。第二个参数传递过来的，是一个空对象
         let res = await reqGetSearchInfo(params)
-        console.log("aaaaa");
+        // console.log("aaaaa");
         if(res.status === 200){
             context.commit('GETSEARCHINFO',res.data)
         }
     }
 }
-const getters = {}
+// vuex中的getters主要用于计算属性，在这里能简化数据
+const getters = {
+    // 里面函数内部有形参，是当前仓库的state
+    goodsList(state){
+        return state.searchList.data.goodsList||[]
+    },
+    trademarkList(state){
+        return state.searchList.data.trademarkList||[]
+    },
+    attrsList(state){
+        return state.searchList.data.attrsList||[]
+    }
+    
+}
 export default {
     state,
     mutations,
